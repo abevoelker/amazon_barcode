5f413205f5c4e90c40e2a8c311fd90d799d4ce10
Abe Voelker <abe@abevoelker.com>
2011-12-10 00:06:20 -0600

    Rails init

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+AmazonBarcode::Application.config.secret_token = '62919f0e6a850331813b453018aef9437be928a824e2e9e2f32eed374df63e9e75aab43f01226f3f59a5da56f3b3880b9fa7fc911e273cf22bb7f26ca632e096'
