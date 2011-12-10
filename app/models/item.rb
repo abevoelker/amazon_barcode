f36a64719c1bfc7b36c20b8b8324e209c39853c1
Abe Voelker <abe@abevoelker.com>
2011-12-10 16:44:48 -0600

    Improve model error handling and add ISBN

--- app/models/item.rb
+++ app/models/item.rb
@@ -7,13 +7,27 @@ require 'barby/outputter/png_outputter'
 class Item < ActiveRecord::Base
   attr_accessible :asin, :barcode
   attr_reader :upc
-  before_create :fetch_upc
+  before_create :get_item_info
 
   def barcode
-    @barcode ||= get_barcode
+    @barcode ||= Barby::Code39.new(upc).to_png if upc
   end
 
   private
+
+  def get_item_info
+    begin
+      @upc = fetch_upc
+      if upc.blank?
+        puts "Sorry, couldn't locate a UPC or ISBN for that item. It may be online-only (e.g. a Kindle book)."
+      else
+        puts "UPC/ISBN is #{upc}"
+      end
+    rescue Exception => e
+      puts "Caught an error: #{e}"
+      # TODO invalidate model (cancel save)
+    end
+  end
 
   def fetch_upc
     req = AmazonProduct["us"]
@@ -23,10 +37,17 @@ class Item < ActiveRecord::Base
       c.tag    = ASSOCIATE_TAG
     end
     res = req.find(asin, :response_group => 'Medium')
-    @upc = res.to_hash['Items']['Item']['ItemAttributes']['UPC']
-  end
-
-  def get_barcode
-    Barby::Code39.new(@upc).to_png
+    valid_req = res.to_hash['Items']['Request']['IsValid'] == 'True' ? true : false
+    raise InvalidRequest and return unless valid_req
+    errors = res.to_hash['Items']['Request']['Errors']
+    if errors && errors.any?
+      if errors['Error']['Code'] == 'AWS.InvalidParameterValue'
+        raise "Invalid ASIN" and return
+      else
+        raise errors['Error']['Message'] and return
+      end
+    end
+    item_attrs = res.to_hash['Items']['Item']['ItemAttributes']
+    item_attrs['UPC'] || item_attrs['ISBN']
   end
 end
